ce4a650d4934aaab411014ffa9a942a576feccbc
Charles-A Despres <cadespres@outlook.com>
2022-06-23 11:11:30 -0400

    Working GET and POST users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,16 @@
-const Console = require("console");
 const console = require("console");
+const User = require("../models/user");
 
 const getAllUsers = (req, res) => {
 
     console.log('Received get users request (no params)')
-    console.log(req.params);
-    console.log(req.body);
-
-    res.json('Ok | GET');
+    User.find()
+        .then(result => {
+            res.json(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 
 const createUser = (req, res) => {
@@ -16,8 +19,17 @@ const createUser = (req, res) => {
     console.log(req.params);
     console.log(req.body);
 
-    res.json('Ok | POST');
+    let user = new User(req.body);
 
+    console.log(user);
+    user.save()
+        .then(result => {
+            res.redirect('/users');
+        })
+        .catch(err => {
+            console.log(err.stack);
+            console.log(err);
+        });
 }
 
 const updateUser = (req, res) => {
